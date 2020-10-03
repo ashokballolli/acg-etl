328b44e36091cff150b66b54ff49162c0456f1ce
Ashok Ballolli <ashok.ballolli123@gmail.com>
2020-10-03 17:46:28 +0200

    rename column name to rep_date

--- etl/load.py
+++ etl/load.py
@@ -9,7 +9,7 @@ setup_logging(CONFIG_FILE)
 
 
 def load_all(dfFinal, db_obj, dwh_conn):
-    dwh_columns = ['reportdate', 'cases', 'deaths', 'recovered']
+    dwh_columns = ['rep_date', 'cases', 'deaths', 'recovered']
     db_obj.insert_data_into_dwh_table(dwh_conn, 'covid', 'daily_stats', dwh_columns, dfFinal, 'dwh')
 
 
@@ -20,17 +20,17 @@ def is_it_first_run(db_obj, dwh_conn):
 
 
 def create_table(db_obj, dwh_conn):
-    query = """CREATE TABLE covid.daily_stats (reportdate date PRIMARY KEY, cases integer, deaths integer, recovered integer)"""
+    query = """CREATE TABLE covid.daily_stats (rep_date date PRIMARY KEY, cases integer, deaths integer, recovered integer)"""
     db_obj.create_table(dwh_conn, query, 'dwh')
 
 
 def load_incremental(df_transformed, db_obj, dwh_conn):
-    query = """SELECT max(reportdate) from covid.daily_stats"""
+    query = """SELECT max(rep_date) from covid.daily_stats"""
     query_results = db_obj.executeQuery(dwh_conn, query, 'dwh')
     max_report_date_dwh = query_results[0][0]
     diff_data = df_transformed.loc[(df_transformed['date'] > str(max_report_date_dwh))]
     print(diff_data.head())
-    dwh_columns = ['reportdate', 'cases', 'deaths', 'recovered']
+    dwh_columns = ['rep_date', 'cases', 'deaths', 'recovered']
     db_obj.insert_data_into_dwh_table(dwh_conn, 'covid', 'daily_stats', dwh_columns, diff_data, 'dwh')
 
 
