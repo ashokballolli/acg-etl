cde39cff714480651f3378b72e196ade969eadc8
Ashok Ballolli <ashok.ballolli123@gmail.com>
2020-10-10 15:02:05 +0200

    create log directory if doesn't exist

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -1,9 +1,10 @@
 import logging
 import yaml
-
+import os
 
 def setup_logging(config_file):
     configs = yaml.load(open(config_file), Loader=yaml.BaseLoader)
     log_config = configs['log']
+    os.makedirs(os.path.dirname(log_config['file_path']), exist_ok=True)
     logging.basicConfig(filename=log_config['file_path'], level=log_config['level'],
                         format='%(asctime)s - %(levelname)s - %(message)s')
