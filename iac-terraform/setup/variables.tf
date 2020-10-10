f6724aa0bf9102b4ca9cdd65958ff3f685a7d052
Ashok Ballolli <ashok.ballolli123@gmail.com>
2020-10-10 14:22:00 +0200

    changing github actions tf structure

--- iac-terraform/setup/variables.tf
+++ iac-terraform/setup/variables.tf
@@ -11,5 +11,5 @@ variable "rds_username" {
 variable "email_addresses" {
   type        = list(string)
   description = "Email address to send notifications to"
-  default = ["write2agb@gmail.com"]
+  default = ["ballolliashok@gmail.com"]
 }
